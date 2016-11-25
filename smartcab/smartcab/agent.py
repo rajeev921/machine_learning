f24152084c1b2ed7d92636928c6904bbd545c37d
rajeev921 <rajeev.cse.imps@gmail.com>
2016-11-25 17:34:45 +0530

    question answered related to q-learning graph

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -8,7 +8,7 @@ class LearningAgent(Agent):
     """ An agent that learns to drive in the Smartcab world.
         This is the object you will be modifying. """ 
 
-    def __init__(self, env, learning=True, epsilon=1.0, alpha=0.5):
+    def __init__(self, env, learning=True, epsilon=0, alpha=0.1):
         super(LearningAgent, self).__init__(env)     # Set the agent in the evironment 
         self.planner = RoutePlanner(self.env, self)  # Create a route planner
         self.valid_actions = self.env.valid_actions  # The set of valid actions
@@ -47,6 +47,7 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
+            #self.epsilon = 1 - (0.05 * self.total_trials)
             self.epsilon = float(1) / math.exp(float(self.a * self.total_trials))
 
         return None
